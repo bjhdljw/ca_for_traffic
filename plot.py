7c4b787c006d0beff91d766bd3db40d03d727868
lijiawen <bjcystl@163.com>
2020-03-23 08:57:38 +0800

    simpifing

--- plot.py
+++ plot.py
@@ -83,77 +83,48 @@ def road_visualization_dynamic(road, time_interval, pause_time):
     '''list for basic_figure'''
 
     plt.ion()
-    fig = plt.figure()
     '''可视化界面布局begin'''
-    ax1 = fig.add_subplot(311)
-    ax2 = fig.add_subplot(334)
-    ax3 = fig.add_subplot(335)
-    # ax4 = fig.add_subplot(336)
-    ax5 = fig.add_subplot(337)
-    # ax6 = fig.add_subplot(338)
-    # ax7 = fig.add_subplot(339)
-    ax2.axis('off')
-    ax3.axis('off')
     time_space_count = 0
     '''可视化界面布局end'''
     for t in range(road.simulation_times):
+        """入口随机入车 begin"""
         for i in range(1, road.lanes + 1):
             carr = car.Car()
             if random.uniform(0, 1) < road.prob_in:
                 # TODO 初始速度的随机分布
                 car.Car.new_car(carr, road, 0, i)
+        """入口随机入车 end"""
         '''每一时间步一展示数据begin'''
-        str_t = 'timestamp= %d' % t
-        str_limit_begin = 'limit begin= %d' % (road.limit_begin + 1)
-        str_limit_end = 'limit end= %d' % (road.limit_end + 1)
-        str_travel_time = 'travel time=%.2f s' % (road.travel_time/road.count_flow if road.count_flow != 0 else 0)
-        travel_speed = (road.travel_speed/road.count_flow if road.count_flow != 0 else 0)
-        str_travel_speed = 'travel speed=%.2f cells/s' % travel_speed
-        str_flow = 'traffic flow= %d' % road.count_flow
-        str_density = 'traffic density= %.2f' % (road.count_flow/travel_speed if travel_speed != 0 else 0)
-        str_switch_counter = 'switch_times= % d' % road.switch_counter
-        ax2.text(0, 0, str_t)
-        ax2.text(0, 0.15, str_flow)
-        ax2.text(0, 0.3, str_limit_begin)
-        ax2.text(0, 0.45, str_limit_end)
-        ax2.text(0, 0.6, str_travel_time)
-        ax2.text(0, 0.75, str_travel_speed)
-        ax2.text(0, 0.9, str_density)
-        ax2.text(0, 1.05, str_switch_counter)
         '''每一时间步一展示数据end'''
         '''每t时间步展示数据begin'''
         if t % time_interval == 0:
+            # 计算当前时间周期内各项数据
+            # 流量
             interval_flow = road.count_flow - temp_flow
             temp_flow = road.count_flow
+            # 速度
             interval_speed = road.travel_speed - temp_speed
             temp_speed = road.travel_speed
             interval_travel_time = road.travel_time - temp_travel_time
             temp_travel_time = road.travel_time
             interval_travel_speed = interval_speed / interval_flow if interval_flow != 0 else 0
-            time_space_y = list()
-            time_space_x = list()
+            # 时空图
+            # time_space_y = list()
+            # time_space_x = list()
             if interval_flow == 0 and interval_speed == 0:
                 pass
             else:
-                time_space_temp = road.positionArray[road.lane_for_st_figure, road.limit_begin : road.limit_end].tolist()
-                for i in range(0, len(time_space_temp)):
-                    if time_space_temp[i] == 0:
-                        time_space_y.append(time_space_temp[i] + time_space_count)
-                        time_space_x.append(i)
+                # time_space_temp = road.positionArray[road.lane_for_st_figure, road.limit_begin : road.limit_end]
+                # .tolist()
+                # for i in range(0, len(time_space_temp)):
+                #     if time_space_temp[i] == 0:
+                #         time_space_y.append(time_space_temp[i] + time_space_count)
+                #         time_space_x.append(i)
                 speed_list.append(interval_speed)
                 flow_list.append(interval_flow)
+                # TODO 密度直接这么算出来可以吗？
                 density_list.append((interval_flow / interval_travel_speed if interval_travel_speed != 0 else 0))
-                time_space_count += 1
-            # ax4.scatter(time_space_x, time_space_y, s=1, c='b')
-            ax5.scatter(flow_list, speed_list, c='b')
-            ax5.set_xlabel('flow')
-            ax5.set_ylabel('speed')
-            # ax6.scatter(density_list, speed_list, c='b')
-            # ax6.set_xlabel('density')
-            # ax6.set_ylabel('speed')
-            # ax7.scatter(density_list, flow_list, c='b')
-            # ax7.set_xlabel('density')
-            # ax7.set_ylabel('flow')
+                # time_space_count += 1
             '''写入CSV'''
             out = open('speed-flow-storage.csv', 'w')
             csv_write = csv.writer(out, dialect='excel')
@@ -166,15 +137,9 @@ def road_visualization_dynamic(road, time_interval, pause_time):
         str_interval_traval_speed = 'interval travel speed= %.2f' % interval_travel_speed
         str_interval_density = 'interval density= %.2f' % (interval_flow/interval_travel_speed if interval_travel_speed != 0 else 0)
         str_interval_travel_time = 'interval travel time= %.2f' % (interval_travel_time/interval_flow if interval_flow != 0 else 0)
-        ax2.text(0, -0.15, str_interval_flow)
-        ax2.text(0, -0.3, str_interval_traval_speed)
-        ax2.text(0, -0.45, str_interval_density)
-        ax2.text(0, -0.6, str_interval_travel_time)
-        ax1.imshow(road.positionArray, cmap=cmap)
-        ax1.axis('off')
+        plt.imshow(road.positionArray, cmap=cmap)
+        plt.axis('off')
         plt.pause(pause_time)
-        ax2.clear()
-        ax2.axis('off')
         road.progress(road.limit_speed)
     plt.ioff()
 
--- road.py
+++ road.py
@@ -15,15 +15,24 @@ PROBSLOW = 0.1
 
 class Road(object):
     def __init__(self):
+        # 从road.conf配置文件获取道路初始化参数
         cp = ConfigParser.SafeConfigParser()
         cp.read('road.conf')
+        # 仿真时长（时间步数）
         self.simulation_times = cp.getint('road', 'simulation_times')
+        # 车道数量
         self.lanes = cp.getint('road', 'lanes')
+        # 车道长度（单位：元胞）
         self.length = cp.getint('road', 'road_length')
+        # 初始化道路信息数组
         self.positionArray = np.zeros((self.lanes + 2, self.length))
+        # 初始化车速信息数组
         self.speedArray = np.zeros((self.lanes + 2, self.length))
+        # 行程时间计数数组（单位：时间步）
         self.speedCounter = np.zeros((self.lanes + 2, self.length))
+        # 停车等待时长数组
         self.timeWaited = np.zeros((self.lanes + 2, self.length))
+        # 初始化道路左右边界
         self.positionArray[0, :] = WALL
         self.positionArray[self.lanes + 1, :] = WALL
         self.speedArray[0, :] = WALL
@@ -32,31 +41,49 @@ class Road(object):
         self.speedCounter[self.lanes + 1, :] = WALL
         self.timeWaited[0, :] = WALL
         self.timeWaited[self.lanes + 1, :] = WALL
+        # 获取车辆最大速度
         self.vmax = cp.getint('road', 'vmax')
+        # 获取进车概率
         self.prob_in = cp.getfloat('road', 'pro_in')
+        # 获取限速开关
         self.is_limit = cp.getboolean('road', 'islimit')
+        # 获取限速区速起始位置
         self.limit_begin = cp.getint('road', 'limit_begin')
+        # 获取限速区结束位置
         self.limit_end = cp.getint('road', 'limit_end')
+        # ？？？
         self.lane_for_st_figure = cp.getint('road', 'lane_for_st_figure')
         self.count_flow = 0
         self.travel_time = 0
         self.travel_speed = 0
+        # 设置换道概率
         self.switch_lane_prob = cp.getint('road', 'switch_lane_prob')
+        # 换道次数统计
         self.switch_counter = 0
+        # 获取限速区限速值
         self.limit_speed = cp.getint('road', 'limit_speed')
+        # 获取事故点开关
         self.iscongestion = cp.getboolean('road', 'iscongestion')
+        # 获取单车道限速值
+        # TODO 车道数量不应该写死
         self.vmax1 = cp.getint('road', 'vmax1')
         self.vmax2 = cp.getint('road', 'vmax2')
         self.vmax3 = cp.getint('road', 'vmax3')
         if self.iscongestion:
+            # 获取事故点车道
             self.congestion_point_lane = cp.getint('road', 'congestion_point_lane')
+            # 获取事故点起始位置
             self.congestion_point_point = cp.getint('road', 'congestion_point_point')
+            # 获取事故点长度
             self.congestion_length = cp.getint('road', 'congestion_length')
+            # 设置道路信息数组的事故点
             self.positionArray[self.congestion_point_lane,
             self.congestion_point_point: self.congestion_point_point + self.congestion_length] = CONGESTION
+        # 获取停车等待时间
         self.time_can_wait = cp.getint('road', 'time_can_wait')
         self.switch_left_prob = cp.getfloat('road', 'switch_left_prob')
 
+    """获取当前车道限制速度"""
     def get_vmax(self, lane):
         if lane == 1:
             return self.vmax1
@@ -65,57 +92,65 @@ class Road(object):
         elif lane == 3:
             return self.vmax3
 
-    def progeress_dqn(self, speed, input_actions):
-        self.progress(speed)
+    # def progeress_dqn(self, speed, input_actions):
+    #     self.progress(speed)
 
+    """道路状态演化函数"""
     def progress(self, speed):
-        '''中间量'''
+        """中间变量 start"""
+        """这些中间变量感觉可以尝试不放在循环里"""
         limit_speed = speed
         positionArray = self.positionArray
         speedArray = self.speedArray
         speedCounter = self.speedCounter
         timeWaited = self.timeWaited
         islimit = False
-        '''中间量'''
-        '''中间量：只有减速步的时候用'''
+        '''中间变量 end'''
+        '''中间变量：减速步使用 start'''
         gap = np.zeros(positionArray.shape)
         left_change_condition = np.zeros(positionArray.shape)
         right_change_condition = np.zeros(positionArray.shape)
         left_change_real = np.zeros(positionArray.shape)
         right_change_real = np.zeros(positionArray.shape)
-        '''中间量：只有减速步的时候用'''
+        '''中间量：减速步使用 end'''
+        """第一个循环：计算gap数组、执行加速步、时间步计数begin"""
         for i in range(1, positionArray.shape[0] - 1):
             gap_position_temp = sys.maxint
+            """从道路最前面的一辆车开始遍历"""
             for j in range(positionArray.shape[1] - 1, -1, -1):
-                '''确定当前的vmax：vmax or limit_speed'''
+                '''确定当前的vmax：vmax or limit_speed start'''
                 vmax = self.get_vmax(i)
                 if positionArray[i, j] == 1:
+                    """如果存在限速区，并且当前车辆位于限速区内，更新最大限制速度"""
                     if positionArray[i, j] == 1 and self.is_limit and self.limit_begin <= j <= self.limit_end:
                         islimit = True
                         vmax = limit_speed
                     else:
                         islimit = False
-                '''确定当前的vmax：vmax or limit_speed'''
+                '''确定当前的vmax：vmax or limit_speed end'''
                 '''加速步骤begin'''
                 speedArray[i, j] = min(speedArray[i, j] + 1, vmax)
                 '''加速步骤end'''
                 '''计算前车距离begin'''
                 if positionArray[i, j] == 1 or positionArray[i, j] == 2:
+                    """检查车辆间距是否计算正确，不正确抛出异常"""
                     if gap_position_temp - j - 1 < 0:
                         raise RuntimeError('gap error')
+                    """如果是道路最前面的一辆车，gap为 sys.maxint - j - 1 """
                     gap[i, j] = gap_position_temp - j - 1
                     gap_position_temp = j
                 '''计算前车距离end'''
-                '''行程开始时间累计begin'''
+                '''行程开始时间累计（单位：时间步）begin'''
                 if self.limit_begin <= j <= self.limit_end and speedCounter[i, j] != 0:
                     speedCounter[i, j] += 1
-                '''行程开始时间累计end'''
-        '''逐车道换道begin'''
+                '''行程开始时间累计（单位：时间步）end'''
+        """第一个循环：计算gap数组、执行加速步、时间步计数end"""
+        '''第二个循环：逐车道换道begin'''
         for i in range(1, self.lanes + 1):
             switch_lane(positionArray, i, self.lanes, self.vmax, right_change_condition, left_change_condition,
                         speedArray, gap, left_change_real, right_change_real, self.switch_lane_prob, speedCounter, self, timeWaited, islimit)
-        '''逐车道换道end'''
-        '''换道后更新前车距离begin'''
+        '''第二个循环：逐车道换道end'''
+        '''第三个循环：换道后更新前车距离begin'''
         gap = np.zeros(positionArray.shape)
         for i in range(1, positionArray.shape[0] - 1):
             gap_position_temp = sys.maxint
@@ -125,11 +160,12 @@ class Road(object):
                         raise RuntimeError('gap error')
                     gap[i, j] = gap_position_temp - j - 1
                     gap_position_temp = j
-        '''换道后更新前车距离end'''
-        '''减速步、随机慢化步begin'''
+        '''第三个循环：换道后更新前车距离end'''
+        '''第四个循环：减速步、随机慢化步begin'''
         for i in range(1, positionArray.shape[0] - 1):
-            # 计数器，第1辆车和第2辆车减速步特殊处理
+            # 计数器，第1辆车和第2辆车减速步特殊处理：第1辆车不需要减速
             count = 1
+            """记录前车距离的临时变量"""
             gap_position_temp2 = sys.maxint
             for j in range(positionArray.shape[1] - 1, -1, -1):
                 '''确定当前的vmax：vmax or limit_speed'''
@@ -139,6 +175,7 @@ class Road(object):
                 if positionArray[i, j] == 1 or positionArray[i, j] == 2:
                     if positionArray[i, j] == 2:
                         pass
+                    # 如果前方不是车辆，而是障碍物的话，不需要考虑前车速度，使用NaSch模型减速步
                     elif gap_position_temp2 < positionArray.shape[1] and positionArray[i, gap_position_temp2] == 2:
                         temp_speed = min(speedArray[i, j], gap[i, j])
                         speedArray[i, j] = max(temp_speed, 0)
@@ -147,6 +184,7 @@ class Road(object):
                     elif count == 2:
                         temp_speed = min(speedArray[i, j], gap[i, j])
                         speedArray[i, j] = max(temp_speed, 0)
+                    # 如果前方是车辆的话，为了更快行进，会考虑前车速度，使用VE减速步
                     else:
                         d = max(0, gap[i, gap_position_temp2] - 1)
                         vmax_before = limit_speed if (self.is_limit and self.limit_begin <= gap_position_temp2 <= self.limit_end) else self.vmax
@@ -161,40 +199,25 @@ class Road(object):
                         raise RuntimeError('v error')
                 '''减速步end'''
                 '''随机慢化步begin'''
-                if (positionArray[i, j] == 1 and random.uniform(0, 1) <= PROBSLOW):
+                if positionArray[i, j] == 1 and random.uniform(0, 1) <= PROBSLOW:
                     speedArray[i, j] = max(speedArray[i, j] - 1, 0)
                 '''随机慢化步end'''
-        '''减速步、随机慢化步end'''
+        '''第四个循环：减速步、随机慢化步end'''
+        '''第五个循环：监测点统计数据、位置更新 begin'''
         for i in range(1, positionArray.shape[0] - 1):
             for j in range(positionArray.shape[1] - 1, -1, -1):
                 '''Nasch位置更新步begin'''
-                if (positionArray[i, j] == 1):
+                if positionArray[i, j] == 1:
                     position_next = int(j + speedArray[i, j])
                     if position_next != j:
-                        if position_next < positionArray.shape[1] and position_next != 1:
-                            for c in range(j + 1, position_next + 1):
-                                if positionArray[i, c] == 1:
-                                    print 'i= %d' % i
-                                    print 'j= %d' % j
-                                    print 'c= %d' % c
-                                    print 'i speed= %d' % speedArray[i, j]
-                                    print 'position_next= %d' % position_next
-                                    raise RuntimeError('car conflict')
-                                if positionArray[i, c] == 2:
-                                    print 'i= %d' % i
-                                    print 'j= %d' % j
-                                    print 'c= %d' % c
-                                    print 'i speed= %d' % speedArray[i, j]
-                                    print 'position_next= %d' % position_next
-                                    print 'gap= %d' % gap[i, j]
-                                    raise RuntimeError('congestion conflict')
                         '''流量&总行程时间&总行程车速统计begin'''
+                        '''限速区末端为计数点'''
                         if j <= self.limit_end <= position_next:
                             self.count_flow += 1
                             self.travel_time += speedCounter[i, j]
                             self.travel_speed += (self.limit_end - self.limit_begin) / speedCounter[i, j]
                         '''流量&总行程时间&总行程车速统计end'''
-                        if (position_next < positionArray.shape[1]):
+                        if position_next < positionArray.shape[1]:
                             positionArray[i, position_next] = 1
                             speedArray[i, position_next] = speedArray[i, j]
                             speedCounter[i, position_next] = speedCounter[i, j]
@@ -206,6 +229,7 @@ class Road(object):
                     elif position_next == j and j != 0:
                         timeWaited[i, j] += 1
                 '''Nasch位置更新步end'''
+        '''第五个循环：监测点统计数据、位置更新 end'''
         return positionArray
 
 
